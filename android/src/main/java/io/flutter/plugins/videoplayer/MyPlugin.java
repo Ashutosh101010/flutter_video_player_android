bef578531127774157b1df054a42e324f444465d
krishnapalsendhav <krishnapalsendhav591@gmail.com>
2023-10-16 17:05:35 +0530

    test 2

--- android/src/main/java/io/flutter/plugins/videoplayer/MyPlugin.java
+++ android/src/main/java/io/flutter/plugins/videoplayer/MyPlugin.java
@@ -1,3 +1,6 @@
+
+package io.flutter.plugins.videoplayer;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
