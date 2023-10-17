73a6615a03d5fc5f9d7d41ae54785743b39aaef3
krishnapalsendhav <krishnapalsendhav591@gmail.com>
2023-10-17 15:29:07 +0530

    test 9

--- android/src/main/java/io/flutter/plugins/videoplayer/MyPlugin.java
+++ android/src/main/java/io/flutter/plugins/videoplayer/MyPlugin.java
@@ -1,55 +1,35 @@
-package io.flutter.plugins.videoplayer;
-
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 
-public class MyPlugin extends FlutterActivity {
-    private static final String CHANNEL = "custom_channel";
-    private static final String CHANNEL2 = "my_plugin";
+public class MyPlugin {
 
-    private static String secretKey ;
+    private final FlutterEngine flutterEngine;
+    private final MethodChannel methodChannel;
 
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),CHANNEL2).setMethodCallHandler((call , result)-> {
-            System.out.println("Hello World from Java");
-            if(call.method.equals("getPlatformVersion")){
-                System.out.println("new Channel In Java");
-            }else{
-                result.notImplemented();
+    public MyPlugin(Context context) {
+        flutterEngine = new FlutterEngine(context);
+        methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "my_channel");
+
+        methodChannel.setMethodCallHandler(new MethodCallHandler() {
+            @Override
+            public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                // Handle the method call from Flutter.
+                System.out.println("Method call from Flutter: " + call.method);
+                if (call.method.equals("myMethod")) {
+                    // Invoke the method here.
+                    System.out.println("Successfully invoked myMethod");
+                    result.success("Hello from Android");
+                } else {
+                    System.out.println("Method not implemented");
+                    result.notImplemented();
+                }
             }
         });
-
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-                .setMethodCallHandler(
-                        (call, result) -> {
-                            // This method is invoked on the main thread.
-                            // TODO
-                            if(call.method.equals("java_method")){
-                                String secretKey = call.argument("secretKey");
-                                System.out.println("Hello From Java");
-                                saveSecretKey(secretKey);
-                                result.success("Success");
-                            }else {
-                                result.notImplemented();
-                                System.out.println("Error in Java Code");
-                            }
-
-
-                        }
-                );
     }
 
-    private void saveSecretKey (String key) {
-        secretKey = key;
-        System.out.println("Secret Key set Successfully : "+getKey());
-
-    }
-
-    public String getKey () {
-        return secretKey;
+    public void invokeMethod(String methodName, Object arguments) {
+        methodChannel.invokeMethod(methodName, arguments);
     }
 }
