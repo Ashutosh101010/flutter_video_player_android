e7792bca0017f92fc4ed0e17e3263f1993d53a97
krishnapalsendhav <krishnapalsendhav591@gmail.com>
2023-10-17 17:24:02 +0530

    test 9

--- android/src/main/java/io/flutter/plugins/videoplayer/MyPlugin.java
+++ android/src/main/java/io/flutter/plugins/videoplayer/MyPlugin.java
@@ -1,35 +1,44 @@
+package io.flutter.plugins.videoplayer;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.embedding.android.FlutterFragment;
 
-public class MyPlugin {
+public class MyPlugin extends FlutterFragment {
 
-    private final FlutterEngine flutterEngine;
-    private final MethodChannel methodChannel;
+    private FlutterEngine flutterEngine;
+    private MethodChannel methodChannel;
 
-    public MyPlugin(Context context) {
-        flutterEngine = new FlutterEngine(context);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        flutterEngine = new FlutterEngine(getContext());
+
         methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "my_channel");
-
         methodChannel.setMethodCallHandler(new MethodCallHandler() {
             @Override
             public void onMethodCall(MethodCall call, MethodChannel.Result result) {
                 // Handle the method call from Flutter.
                 System.out.println("Method call from Flutter: " + call.method);
-                if (call.method.equals("myMethod")) {
-                    // Invoke the method here.
-                    System.out.println("Successfully invoked myMethod");
-                    result.success("Hello from Android");
-                } else {
-                    System.out.println("Method not implemented");
-                    result.notImplemented();
-                }
             }
         });
     }
 
-    public void invokeMethod(String methodName, Object arguments) {
-        methodChannel.invokeMethod(methodName, arguments);
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        FlutterView flutterView = (FlutterView) inflater.inflate(R.layout.fragment_flutter, container, false);
+        flutterView.attachToFlutterEngine(flutterEngine);
+
+        return flutterView;
     }
 }
