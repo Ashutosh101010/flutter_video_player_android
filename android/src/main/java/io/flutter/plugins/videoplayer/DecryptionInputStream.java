be71349024d8d26943da16bbdaf6f2d1a35e2f13
krishnapalsendhav <krishnapalsendhav591@gmail.com>
2023-10-25 15:07:46 +0530

    test 33

--- android/src/main/java/io/flutter/plugins/videoplayer/DecryptionInputStream.java
+++ android/src/main/java/io/flutter/plugins/videoplayer/DecryptionInputStream.java
@@ -47,7 +47,7 @@ public class DecryptionInputStream extends InputStream {
 
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
-      System.out.println("read byte[] b, int off, int len hhhhhhhhhhh" +" "+off+" "+len );
+      System.out.println("read byte[] b, int off, int len hhhhhhhhhhh" +" "+b.length+off+" "+len );
 
         try {
             byte[] decryptedBuffer = cipher.doFinal(b, off, len);
